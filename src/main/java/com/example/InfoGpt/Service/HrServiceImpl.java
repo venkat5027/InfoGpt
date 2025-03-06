bb8cf4c1b9cb0164b3f8a6a16b692cb0857f7fcc
sangamnaidu <vambarasangamnaidu@gmail.com>
2025-03-06 12:30:00 +0530

    hr name changes

--- src/main/java/com/example/InfoGpt/Service/HrServiceImpl.java
+++ src/main/java/com/example/InfoGpt/Service/HrServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import com.example.InfoGpt.Enums.Gender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,13 +28,23 @@ public class HrServiceImpl implements InfoGpt {
 
 	@Override
 	public ResponseEntity<?> getDetails(String name, FacultyAndHrQueryType type, OrganizationQueryType orgType) {
+		StringBuffer responseStr;
 		switch (type) {
 		case NAME:
-			Optional<HR> faculty = hrRepository.findByName(name);
-			if (faculty.isEmpty())
+			Optional<HR> hr = hrRepository.findByName(name);
+			if (hr.isEmpty())
 				return ResponseEntity.status(HttpStatus.NOT_FOUND)
 						.body(Map.of("response", "The data is not found for the requested hr name " + name));
-			return ResponseEntity.ok(faculty);
+			responseStr = new StringBuffer();
+			responseStr.append("The requested details of " + name + ": ");
+			if (hr.get().getGender().toUpperCase().equals(Gender.MALE.name()))
+				responseStr.append("He is ");
+			else
+				responseStr.append("she is ");
+			responseStr.append(hr.get().getAge())
+					.append(" years old and has " + hr.get().getExperience() + "+ years of experience at "
+							+ hr.get().getOrganizationName());
+			return ResponseEntity.ok(Map.of("response", responseStr));
 		case ORGNAME:
 			Optional<Organization> org = organizationRepository.findByName(name);
 			if (org.isEmpty())
